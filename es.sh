4336a785f2219edb237a913501dc4df878d78fed
Rock Yu <chenzhong_yu@hotmail.com>
2020-08-22 22:31:00 +1000

    - Remove heap command, heap info is made available in default _cat/nodes api - Add "columns" command to let use ask for all the columns a _cat api supports: e.g:  es columns indices - Add `-c | --columns` to allow user filter the columns

--- es.sh
+++ es.sh
@@ -6,32 +6,35 @@ _usage()
   cat <<EOF_USAGE
 Usage: $0 [options] <command>
 
-Available commands:
+Available _cat commands:
    allocation  Manage shard allocation
    health      Cluster health
-   indices     Show elasticsearch indices
+   indices     Show indices
    master      List master node
    nodes       Node stats
-   heap        Heap statistics for nodes (deprecated, use "es nodes" instread)
    shards      List shards in the cluster
    snapshots   List snapshots that belong to a repository (default to "backup_repo" or use --snapshot-repo to override)
+
+Extra command:
    version     Show cluster name and current elastic build version
 
 Options:
    -u  --url <url>   elastic url (default "http://localhost:9200")
+   -c  --columns     comma-separated columns to return
    -v  --verbose     print column headers
    --snapshot-repo   specify repository to use by "snapshots" command
 
+Use '$0 columns <command>' to list columns the _cat command supports
 EOF_USAGE
 }
 
+es_url="${es_url:-http://localhost:9200}"
 # Do not print column headers by default
 verbose=false
+ask_columns=false
 command=""
-
 # Comma-separated list of column names to display
-headers_filter=""
-
+columns=""
 snapshot_repo="backup_repo"
 
 while [ "$1" != "" ]; do
@@ -39,6 +42,9 @@ while [ "$1" != "" ]; do
         -u | --url )            shift
                                 es_url=$1
                                 ;;
+        -c | --columns )        shift
+                                columns=$1
+                                ;;                                
         -v | --verbose )        verbose=true
                                 ;;
         --snapshot-repo )       shift
@@ -46,20 +52,16 @@ while [ "$1" != "" ]; do
                                 ;;
         -h | --help )           _usage
                                 exit
+                                ;;                                
+        column | columns )      shift
+                                ask_columns=true
+                                command=$1
                                 ;;
-        allocation | health | indices | master | shards | aliases | version)
+        nodes | allocation | health | indices | master | shards | aliases | version)
                                 command=$1
                                 ;;
         snapshots )
                                 command="snapshots"
-                                ;;
-        nodes )
-                                command="nodes"
-                                headers_filter="&h=name,ip,master,heap.current,heap.percent,ram.current,ram.max,ram.percent,disk.used,disk.total,disk.used_percent"
-                                ;;
-        heap )
-                                command="nodes"
-                                headers_filter="&h=name,ip,master,heap.current,heap.percent"
                                 ;;
         * )                     _usage
                                 exit 1
@@ -67,7 +69,7 @@ while [ "$1" != "" ]; do
     shift
 done
 
-es_url="${es_url:-http://localhost:9200}"
+
 if [ -z "$command" ]; then
     _usage
     echo "<command> should be provided"
@@ -77,8 +79,19 @@ fi
 if [[ "$command" = "version" ]]; then
   #rename number to version_number | remove quotes (") and commas (,) | removing leading/tailing spaces
   curl -s "$es_url" | grep 'cluster_name\|number' | sed 's/number/version_number/g' | tr -d '"' | tr -d ',' | tr -d ' '
-elif [[ "$command" = "snapshots" ]]; then
+  exit 0
+fi
+
+# ask command columns 
+if [[ "$ask_columns" = "true" ]]; then
+  curl -s "$es_url/_cat/$command?help"
+  exit 0
+fi
+
+# cat API
+if [[ "$command" = "snapshots" ]]; then
     curl -s "$es_url/_cat/$command/$snapshot_repo?v=${verbose}"
 else
+  [[ -n "$columns" ]] && headers_filter="&h=${columns}" 
   curl -s "$es_url/_cat/$command?v=${verbose}${headers_filter}"
 fi
