943eb13850e03a415d04c966b96d3e85d9035eeb
Rock YU <rockyu@aconex.com>
2020-08-24 18:02:18 +1000

    update help text + use `-l|--columns to query supported _cat columns`

--- es.sh
+++ es.sh
@@ -4,7 +4,7 @@
 _usage()
 {
   cat <<EOF_USAGE
-Usage: $0 [options] <command>
+Usage: $0 [-u|--url URL] [-c|--columns COLUMNS] [-v|--verbose] <command>
 
 Available _cat commands:
    allocation  Manage shard allocation
@@ -15,16 +15,54 @@ Available _cat commands:
    shards      List shards in the cluster
    snapshots   List snapshots that belong to a repository (default to "backup_repo" or use --snapshot-repo to override)
 
-Extra command:
+Extra command (not _cat):
    version     Show cluster name and current elastic build version
 
 Options:
+   -h --help         show this help text
    -u  --url <url>   elastic url (default "http://localhost:9200")
-   -c  --columns     comma-separated columns to return
+   -c  --columns     comma-separated columns to return for the _cat command
+   -l  --list-columns   list the columns supported by the _cat command 
    -v  --verbose     print column headers
    --snapshot-repo   specify repository to use by "snapshots" command
 
-Use '$0 columns <command>' to list columns the _cat command supports
+
+Examples:
+  List nodes in cluster:
+
+    $ es -v nodes
+    ip         heap.percent ram.percent cpu load_1m load_5m load_15m node.role master name
+    172.19.0.2           56          42   1    0.00    0.04     0.00 mdi       -      cu_4t7K
+    172.19.0.3           70          42   1    0.00    0.04     0.00 mdi       *      WI7hZOS
+
+
+  List nodes in the cluster, only return ip, ram.current, load_1m columns
+
+    $ es -v -c ip,ram.current,load_1m  nodes
+    ip         ram.current load_1m
+    172.19.0.2       3.2gb    0.01
+    172.19.0.3       3.2gb    0.01
+
+
+  To get all columns supported by the `nodes` command
+
+    $ es -l nodes
+
+    id                           | id,nodeId                      | unique node id
+    disk.used                    | du,diskUsed                    | used disk space
+    disk.avail                   | d,da,disk,diskAvail            | available disk space
+    disk.used_percent            | dup,diskUsedPercent            | used disk space percentage
+    heap.current                 | hc,heapCurrent                 | used heap
+    heap.percent                 | hp,heapPercent                 | used heap ratio
+    heap.max                     | hm,heapMax                     | max configured heap
+    ram.current                  | rc,ramCurrent                  | used machine memory
+    ram.percent                  | rp,ramPercent                  | used machine memory ratio
+    ram.max                      | rm,ramMax                      | total machine memory
+    cpu                          | cpu                            | recent cpu usage
+    load_1m                      | l                              | 1m load avg
+    load_5m                      | l                              | 5m load avg
+    -- remainings ignored 
+
 EOF_USAGE
 }
 
@@ -53,7 +91,7 @@ while [ "$1" != "" ]; do
         -h | --help )           _usage
                                 exit
                                 ;;                                
-        column | columns )      shift
+        -l | --list-columns )   shift
                                 ask_columns=true
                                 command=$1
                                 ;;
